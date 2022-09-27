60d769c2740fabf4e45371abc64883d58efe529a
CLOUDFRAMEWORK <devteam@cloudframework.io>
2022-09-27 11:48:31 +0200

    Adding DB connection with secrets

--- api/04-sql/00-connect.php
+++ api/04-sql/00-connect.php
@@ -55,11 +55,45 @@ class API extends RESTful
         $config['dbProxy'] = $this->getFormParamater('dbProxy')?:null;
         $config['dbProxyHeaders'] = $this->getFormParamater('dbProxyHeaders')?:null;
         $config['dbCharset'] = $this->getFormParamater('dbCharset')?:null;
-        $config['dbPort'] = $this->getFormParamater('dbPort')?:null;
+        $config['dbPort'] = $this->getFormParamater('dbPort')?:'3306';
         //endregion
 
         //region SET $this->core->config->processConfigData($config);
         $this->core->config->processConfigData($config);
+        //endregion
+
+        //region CREAT $sql CloudSQL object.
+        /** @var CloudSQL $sql */
+        $sql = $this->core->loadClass('CloudSQL');
+        if($sql->error()) return $this->setErrorFromCodelib('system-error',$sql->getError());
+        //endregion
+
+        //region CONNECT and execute SET $query_result with the result of the query: SELECT count(*) from test
+
+        // By default $sql->connect will search for config vards: dbUser,dbPassword etc..
+        if(!$sql->connect()) return $this->setErrorFromCodelib('system-error',$sql->getError());
+
+        // Also we can establish the connection by parameter
+        //if(!$sql->connect($config['dbServer'],$config['dbUser'],$config['dbPassword'],$config['dbName'],$config['dbPort'],$config['dbSocket'],$config['dbCharset'])) return $this->setErrorFromCodelib('system-error',$sql->getError());
+
+        $query_result = $sql->getDataFromQuery('SELECT count(*) from test');
+        if($sql->error()) return $this->setErrorFromCodelib('system-error',$sql->getError());
+        $sql->close();
+        //endregion
+
+        //region RETURN result
+        $this->addReturnData(['SELECT count(*) from test'=>$query_result]);
+        //endregion
+
+    }
+
+    /**
+     * Endpoint to show Hello World message
+     */
+    public function ENDPOINT_fetch()
+    {
+        //region SET DB Connection
+        if(!$this->assignEnvDBVariables()) return;
         //endregion
 
         //region CREAT $sql CloudSQL object.
@@ -77,6 +111,25 @@ class API extends RESTful
         //region RETURN result
         $this->addReturnData(['SELECT count(*) from test'=>$query_result]);
         //endregion
+    }
 
+    /**
+     * Assign DB environment variables as secrets to connect with Database
+     * There are different ways
+     */
+    private function assignEnvDBVariables() {
+        //region FEED $config with mandatory params
+        $config = [];
+        if(!$config['dbServer'] = $this->core->config->getEnvVar('ACADEMY_DB_SERVER')) return $this->setErrorFromCodelib('system-error','MISSING ACADEMY_DB_NAME env-var');
+        if(!$config['dbUser'] = $this->core->config->getEnvVar('ACADEMY_DB_USER')) return $this->setErrorFromCodelib('system-error','MISSING ACADEMY_DB_USER env-var');
+        if(!$config['dbPassword'] = $this->core->config->getEnvVar('ACADEMY_DB_PASSWORD')) return $this->setErrorFromCodelib('system-error','MISSING dbPassword env-var');
+        if(!$config['dbName'] = $this->core->config->getEnvVar('ACADEMY_DB_NAME')) return $this->setErrorFromCodelib('system-error','MISSING ACADEMY_DB_NAME env-var');
+        //endregion
+
+        //region SET $this->core->config->processConfigData($config);
+        $this->core->config->processConfigData($config);
+        //endregion
+
+        return true;
     }
 }
